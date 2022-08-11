68639eb22314a1adfad722e3853c80916ca1286e
Joaquin Benitez <joaquincdelu@gmail.com>
2022-08-11 15:31:05 -0300

    entrega 2

--- js/main.js
+++ js/main.js
@@ -1,75 +1,213 @@
-function servicio(nombre, precio, stock){
-    this.nombre = nombre
-    this.precio = precio
-    this.stock = stock
+document.addEventListener('DOMContentLoaded', () => {
+    let carrito = [];
+    const DOMitems = document.getElementById('items');
+    const DOMcarrito = document.querySelector('#carrito');
+    const DOMtotal = document.querySelector('#total');
+    const DOMbotonVaciar = document.querySelector('#boton-vaciar');
+    const listaDeProductos = [
+        {
+            id: 1,
+            nombre: "Masaje de Cutis",
+            precio: 1000,
+            img:'ImgSpa/cutis.jpg'
+        },
+        {
+            id: 2,
+            nombre: "Masaje con piedras",
+            precio: 1400,
+            img:'ImgSpa/piedras.jpg'
+        },
+        {
+            id: 3,
+            nombre: "Masaje Espalda",
+            precio: 1200,
+            img:'ImgSpa/espalda.jpg'
+        },
+        {
+            id: 4,
+            nombre: "Masaje de Pies",
+            precio: 1300,
+            img:'ImgSpa/pies.jpg'
+        },
+        {
+            id: 5,
+            nombre: "Masaje de Piernas",
+            precio: 1200,
+            img:'ImgSpa/piernas.jpg'
+        },
+        {
+            id: 6,
+            nombre: "Cuerpo Completo",
+            precio: 2000,
+            img:'ImgSpa/masaje.jpg'
+        },
+        {
+            id: 7,
+            nombre: "Masaje con pindas",
+            precio: 1000,
+            img:'ImgSpa/pindas.jpg'
+        },
+        {
+            id: 8,
+            nombre: "Fangoterapia",
+            precio: 1250,
+            img:'ImgSpa/fango.jpg'
+        },
+        {
+            id: 9,
+            nombre: "Masaje Exfoliante",
+            precio: 1500,
+            img:'ImgSpa/espalda-exfoliante.webp'
+        },
+        {
+            id: 10,
+            nombre: "Ventosas",
+            precio: 1250,
+            img:'ImgSpa/ventosas.jpg'
+        }
+        
+    ]
     
-  }
-  
-  const servicio1 = new servicio("MasajeEspalda", 1400, 10)
-  const servicio2 = new servicio("MasajePiernas", 1200, 10)
-  const servicio3 = new servicio("MasajeCara", 1400, 10)
-  const servicio4 = new servicio("MasajePiedras", 1000, 10)
-  
-
-let listaServicios = [servicio1, servicio2, servicio3, servicio4]
-let nombresServicios = listaServicios.map((servicio) => servicio.nombre)
-let cantidadCompras = prompt("Ingrese la cantidad de servicios distintos que desea comprar:\n " + nombresServicios.join("\n "))
-
-let precioTotal = 0;
-
-function calculoPrecio(cantidad, precio){
-    precioTotal += cantidad * precio
-}
-
-function calculoStock(cantidad, servicio){
-    if(servicio.stock >= cantidad){
-        calculoPrecio(cantidad, servicio.precio)
-        alert("El precio total es de: $" + (cantidad * servicio.precio))
+    function renderizarProductos() {
+        listaDeProductos.forEach((info) => {
+            // Estructura
+            const miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-sm-4');
+            // Body
+            const miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body');
+            // Titulo
+            const miNodoTitle = document.createElement('h5');
+            miNodoTitle.classList.add('card-title');
+            miNodoTitle.innerText = info.nombre;
+              // Imagen
+            const miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('imagen');
+            miNodoImagen.setAttribute('src', info.img);
+            // Precio
+            const miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.innerText = `${info.precio}`;
+            miNodoPrecio.innerText = `$${info.precio}`;
+    
+            const miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-primary');
+            miNodoBoton.innerText = 'agregar al carrito';
+            miNodoBoton.setAttribute('marcador', info.id);
+            miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+            // Insertamos
+            miNodoCardBody.append(miNodoImagen);
+            miNodoCardBody.append(miNodoTitle);
+            miNodoCardBody.append(miNodoPrecio);
+            
+            miNodoCardBody.append(miNodoBoton);
+            miNodo.append(miNodoCardBody);
+            DOMitems.append(miNodo);
+        });
     }
-    else{
-        alert("No es posible brindarle tantos servicio, selecione una cantidad menor")
+    /**
+    * Evento para añadir un producto al carrito de la compra
+    */
+    function anyadirProductoAlCarrito(e) {
+        // Anyadimos el Nodo a nuestro carrito
+        carrito.push(e.target.getAttribute('marcador'))
+        // Actualizamos el carrito 
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
     }
-}
-
-for(let i = 0; i < cantidadCompras; i++){
-    let compra1 = prompt("Ingrese el nombre del servicio que quiere comprar:\n " + nombresServicios.join("\n ")).toLowerCase()
-    let cantidad1 = prompt("ingrese la cantidad del servicios que quiere comprar:");
-    if(compra1 == servicio1.nombre.toLowerCase()){
-        calculoStock(cantidad1, servicio1)
+    /**
+    * Dibuja todos los productos guardados en el carrito
+    */
+    function renderizarCarrito() {
+        // Vaciamos todo el html
+        DOMcarrito.innerText = '';
+        // Quitamos los duplicados
+        const carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach((item) => {
+            // Obtenemos el item que necesitamos de la variable base de datos
+            const miItem = listaDeProductos.filter((itemBaseDatos) => {
+                // ¿Coincide las id? Solo puede existir un caso
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Cuenta el número de veces que se repite el producto
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            // Creamos el nodo del item del carrito
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.innerText = `${numeroUnidadesItem} x ${miItem[0].nombre} - $${miItem[0].precio}`;
+            // Boton de borrar
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.innerText = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            // Mezclamos nodos
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        });
+        // Renderizamos el precio total en el HTML
+        DOMtotal.innerText = calcularTotal();
     }
-    else if(compra1 == servicio2.nombre.toLowerCase()){
-        calculoStock(cantidad1, servicio2)
+    /**
+    * Evento para borrar un elemento del carrito
+    */
+    function borrarItemCarrito(e) {
+        // Obtenemos el producto ID que hay en el boton pulsado
+        const id = e.target.dataset.item;
+        // Borramos todos los productos
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        // volvemos a renderizar
+        renderizarCarrito();
+        // Actualizamos el LocalStorage
+        guardarCarritoEnLocalStorage();
     }
-    else if(compra1 == servicio3.nombre.toLowerCase()){
-        calculoStock(cantidad1, servicio3)
+    /**
+     * Calcula el precio total teniendo en cuenta los productos repetidos
+     */
+    function calcularTotal() {
+        // Recorremos el array del carrito 
+        return carrito.reduce((total, item) => {
+            // De cada elemento obtenemos su precio
+            const miItem = listaDeProductos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
+            // Los sumamos al total
+            return total + miItem[0].precio;
+        }, 0).toFixed(2);
     }
-    else if(compra1 == servicio4.nombre.toLowerCase()){
-        calculoStock(cantidad1, servicio4)
+    /**
+    * Varia el carrito y vuelve a dibujarlo
+    */
+    function vaciarCarrito() {
+        // Limpiamos los productos guardados
+        carrito = [];
+        // Renderizamos los cambios
+        renderizarCarrito();
+        // Borra LocalStorage
+        localStorage.removeItem('carrito');
     }
-
-    else{
-    alert("No tenemos ese servicio")
+    function guardarCarritoEnLocalStorage () {
+        localStorage.setItem('carrito', JSON.stringify(carrito));
     }
-}
-
-switch(true){
-    case precioTotal < 3000 && precioTotal > 1000:
-        precioTotal = precioTotal * 0.95
-        alert("Recibiste un descuento del 5% por tu compra")
-        break;
-    case precioTotal >= 3000 && precioTotal <6000:
-        precioTotal = precioTotal * 0.90
-        alert("Recibiste un descuento del 10% por tu compra")
-        break;
-    case precioTotal >= 6000:
-        alert("Recibiste un descuento del 30% por tu compra")
-        precioTotal = precioTotal * 0.70
-        break;
-    default:
-        console.log(precioTotal)
-        alert("No ingresaste un precio en numeros");
-        break;
-}
-
-
-alert("Este es el precio total final de tu compra: $" + precioTotal) 
+    function cargarCarritoDeLocalStorage () {
+        // ¿Existe un carrito previo guardado en LocalStorage?
+        if (localStorage.getItem('carrito') !== null) {
+            // Carga la información
+            carrito = JSON.parse(localStorage.getItem('carrito'));
+        }
+    }
+    // Eventos
+    DOMbotonVaciar.addEventListener('click', vaciarCarrito);
+    // Inicio
+    cargarCarritoDeLocalStorage();
+    renderizarProductos();
+    renderizarCarrito();
+});
